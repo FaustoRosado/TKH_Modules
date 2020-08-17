3b15ce207c38ddca0f84391329476a890dafacb2
Fausto Rosado <faustrosado@gmail.com>
2020-08-16 23:39:18 -0400

    module5 challenge3 changes

--- Module_5_Programming1/coding_challenges/challenge_3.js
+++ Module_5_Programming1/coding_challenges/challenge_3.js
@@ -3,7 +3,7 @@
 let quote = "Wipe the sweat off my dome, spit the phlegm on the streets Suede Timbs on my feets makes my cipher complete Whether cruising in a Sikh's cab, or Montero Jeep I can't call it, the beats make me falling asleep I keep falling, but never falling six feet deep I'm out for presidents to represent me, I'm out for presidents to represent me, I'm out for dead presidents to represent me, "
 
 let splitLyric = quote.split(' ');
-console.log(splitLyric);
+//console.log(splitLyric);
 
 let newLyric = [];
 
@@ -11,5 +11,6 @@ for (let i = 0; i < splitLyric.length; i++) {
     newLyric.push(splitLyric[i].replace(/s/g, "$").replace(/S/g, "$"));
 }
 
-console.log(newLyric);
-console.log(newLyric.join(" "));
+let transformQuote = newLyric.join(" ");
+//console.log(newLyric);
+console.log(transformQuote);
